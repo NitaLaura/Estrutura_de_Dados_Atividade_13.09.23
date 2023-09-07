b942205d381252333a50b26230336e92bceefdfc
Laura <laauraanitaa@gmail.com>
2023-09-07 15:46:35 -0300

    Adicionando Exercícios

---
+++ Atividade 13.09/algoritmo481/src/main/java/com/mycompany/algoritmo481/Algoritmo481.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.algoritmo481;
+
+/**
+ *
+ * @author Nyta
+ */
+public class Algoritmo481 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
